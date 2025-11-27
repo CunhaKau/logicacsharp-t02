14255e829cf4f9554948e67c42512ccdb285915a
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-11-27 16:54:05 -0300

    funcoes

---
+++ formulario01/Program.cs
@@ -0,0 +1,21 @@
+﻿//Console.WriteLine("Digite um número: ");
+//int n1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite outro número");
+//int n2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("O resultado é " + (n1+ n2));
+
+//List<string> nomes = new List<string>();
+//nomes.Add("Jorge");
+//nomes.Add("Mario");
+//nomes.Add("Paula");
+//nomes.Remove("Mario");
+//nomes[0] = "Pedro";
+//nomes.Add("Maria");
+//nomes.RemoveAt(2);
+//Console.WriteLine(nomes[0]);
+//Console.WriteLine(nomes[1]);
+
+//Console.WriteLine("Digite o ano em que você nasceu: ");
+//int idade = 2024 - int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Sua idade é de " + idade + " anos");
