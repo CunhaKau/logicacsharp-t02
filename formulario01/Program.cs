d720326eab1c2bf3497c0b6ffd9680717911f3be
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-12-04 16:48:44 -0300

    classes e listas

--- formulario01/Program.cs
+++ formulario01/Program.cs
@@ -1,21 +1,71 @@
-﻿//Console.WriteLine("Digite um número: ");
-//int n1 = int.Parse(Console.ReadLine());
-//Console.WriteLine("Digite outro número");
-//int n2 = int.Parse(Console.ReadLine());
-//Console.WriteLine("O resultado é " + (n1+ n2));
+﻿//Resposta exercício 2 - letra B
 
-//List<string> nomes = new List<string>();
-//nomes.Add("Jorge");
-//nomes.Add("Mario");
-//nomes.Add("Paula");
-//nomes.Remove("Mario");
-//nomes[0] = "Pedro";
-//nomes.Add("Maria");
-//nomes.RemoveAt(2);
-//Console.WriteLine(nomes[0]);
-//Console.WriteLine(nomes[1]);
+Console.WriteLine("Digite um número: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro número");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("O resultado é " + (n1+ n2));
+
+
+//Resposta do exercício 3 - letra D
+
+List<string> nomes = new List<string>();
+nomes.Add("Jorge");
+nomes.Add("Mario");
+nomes.Add("Paula");
+nomes.Remove("Mario");
+nomes[0] = "Pedro";
+nomes.Add("Maria");
+nomes.RemoveAt(2);
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+
+
+//Resposta do exercício 4 - C
 
 //Console.WriteLine("Digite o ano em que você nasceu: ");
-//int idade = 2024 - int.Parse(Console.ReadLine());
+int idade = 2024 - int.Parse(Console.ReadLine());
 
-//Console.WriteLine("Sua idade é de " + idade + " anos");
+Console.WriteLine("Sua idade é de " + idade + " anos");
+
+
+//Resposta do exercício 5 - Letra C
+
+string nome = "Pedro";
+
+
+
+//Resposta do exercício 6 - letra C
+int capacidadeTanque = 100;
+while (capacidadeTanque > 0)
+{
+    Console.WriteLine("Quantidade de água restante: " + capacidadeTanque + "litros");
+    capacidadeTanque -=  3;
+}
+
+//Resposta do exercício 7
+string[] listaDeCompras = { "Pão", "Leite", "Ovos", "Queijo", "Café" };
+string terceiroItem = listaDeCompras[2];
+Console.WriteLine("O terceiro item da lista de compras é: " + terceiroItem);
+
+//Resposta do exercício 8
+bool logado = true;
+
+//Resposta do exercício 9 - Letra A
+string nome = "Eduardo";
+string sobrenome = "da Silva";
+string nome_completo = nome + " " + sobrenome;
+Console.WriteLine(nome_completo);
+
+//Resposta do exercício 10 
+int a = 15;
+int b = 5;
+int resultado = 0;
+
+while(a > 0)
+{
+    a -= b;
+    resultado++;
+}
+
+Console.WriteLine("o resultado é: " + resultado);
--- formulario01/obj/Debug/net8.0/formulario01.AssemblyInfo.cs
+++ formulario01/obj/Debug/net8.0/formulario01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("formulario01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae803d0f41ae08a80f7cd2f1fd81b188dcd4536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14255e829cf4f9554948e67c42512ccdb285915a")]
 [assembly: System.Reflection.AssemblyProductAttribute("formulario01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("formulario01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
